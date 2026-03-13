5f9883669685a35d849884341fa902b62cdd80be
mflix24 <banglacourse1223@gmail.com>
2026-03-13 12:25:08 +0000

    Few files added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating Directories
+# mkdir -p src
+# mkdir -p research
+
+
+touch .env
+touch news_fetcher.py
+touch app.py
+touch requirements.txt
+touch ai_generator.py
+touch publisher.py
+
+echo "Directory and Files Created Successfully"
+
+
